bd6e5ead6188f019ce1e65f9a488f94010cfb90c
Mikhail Tsiberev <mikhail.tsiberev@firstlinesoftware.com>
2015-03-22 20:14:58 +0300

    added IoC

--- Organizations/Program.cs
+++ Organizations/Program.cs
@@ -6,10 +6,41 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using StructureMap;
+using StructureMap.Configuration.DSL;
 
 
 namespace Organizations
 {
+    public class RegisterAsPlugin
+    {
+        public IContainer Container;
+
+        public RegisterAsPlugin()
+        {
+            Container = new Container(x =>
+            {
+                x.For<IRepository<Organization>>().Use<Repository<Organization>>().Named("Organization");
+                x.For<IRepository<Department>>().Use<Repository<Department>>().Named("Department");
+                x.For<IRepository<Employee>>().Use<Repository<Employee>>().Named("Employee"); ;
+            });
+        }
+    }
+    
+    public class RegisterByContainer
+    {
+        public IContainer Container;
+
+        public RegisterByContainer()
+        {
+            Container = new Container(x =>
+            {
+                x.For<IRepository<Organization>>().Use<Repository<Organization>>();
+                x.For<IRepository<Department>>().Use<Repository<Department>>();
+                x.For<IRepository<Employee>>().Use<Repository<Employee>>();
+            });
+        }
+    }
 
     class Program
     {
@@ -35,6 +66,18 @@ namespace Organizations
           
             for (int i = 0; i < 5; i++)
                 report.ShowRandomEmployee();
+
+            var container = new RegisterAsPlugin().Container;
+            var employeeRepositoryTest = container.GetInstance<IRepository<Employee>>("Employee");
+            
+            var container2 = new RegisterByContainer().Container;
+            var organizationRepository = container2.GetInstance<IRepository<Organization>>();
+            var departmentRepository = container2.GetInstance<IRepository<Department>>();
+            var employeeRepository = container2.GetInstance<IRepository<Employee>>();
+            
+            var facc = new Facade(organizationRepository, departmentRepository, employeeRepository);
+            facc.Init();
+            facc.GetAllEmployees();
         }
 
     }
