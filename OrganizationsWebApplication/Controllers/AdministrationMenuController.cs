6a15bc0ff23a70e9d86ba9a91f871a727eefd219
Mikhail Tsiberev <mikhail.tsiberev@firstlinesoftware.com>
2015-07-20 00:24:42 +0300

    - fixed changing list/grid view - added Administration Controller, user Model (Administration feature not implemented)

---
+++ OrganizationsWebApplication/Controllers/AdministrationMenuController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Organizations;
+using OrganizationsWebApplication.Models;
+using OrganizationsWebApplication.MvcHelpers;
+
+namespace OrganizationsWebApplication.Controllers
+{
+    public class AdministrationMenuController : Controller
+    {
+        //
+        // GET: /AdministrationMenu/
+        private Facade m_facade = RegisterByContainer.Container.GetInstance<Facade>();
+
+        public ActionResult AddNewUserMenu()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ChangeRolesMenu()
+        {
+            var user = new User("Petrov", "admin");
+            return View(user);
+        }
+        
+        [HttpPost]
+        public ActionResult ChangeRolesMenu(User us)
+        {
+            return View(us);
+        }
+
+
+
+    }
+}
---
+++ OrganizationsWebApplication/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrganizationsWebApplication.Models
+{
+    public class User
+    {
+        public User(string name, string role)
+        {
+            Name = name;
+            Role = role;
+        }
+
+        public string Name { get; private set; }
+        public string Role { get; private set; }
+    }
+}
